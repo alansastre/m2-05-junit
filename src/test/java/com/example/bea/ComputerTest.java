a0290e8ef0872f791aeda03d16426dd3daeddf6e
Bcorral <beatrizcorralcampos@gmail.com>
2021-07-15 22:06:55 +0200

    commit inicial creacion de rama feature/bea

---
+++ src/test/java/com/example/bea/ComputerTest.java
@@ -0,0 +1,43 @@
+package com.example.bea;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.patterns.behavioral.observer.Computer;
+import com.example.demo.patterns.behavioral.observer.WeatherObserver;
+import com.example.demo.patterns.behavioral.observer.WeatherType;
+
+class ComputerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("Comprobar el nombre del tiempo es correcto")
+	void testTipoTiempo() {
+		
+		
+	}
+	
+}	
+	
+	
