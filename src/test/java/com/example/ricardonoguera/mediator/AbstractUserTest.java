bcf264b02abf4830da266646b15615c4c82b4516
Ricardo <rnogueradrum@gmail.com>
2021-07-15 21:00:51 +0200

    commit test clase AbstractUser

--- src/test/java/com/example/ricardonoguera/mediator/AbstractUserTest.java
+++ src/test/java/com/example/ricardonoguera/mediator/AbstractUserTest.java
@@ -2,6 +2,7 @@ package com.example.ricardonoguera.mediator;
 
 import com.example.demo.patterns.behavioral.mediator.*;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
@@ -14,25 +15,59 @@ import org.junit.jupiter.api.Test;
 
 
 class AbstractUserTest {
+	// Cambiar visibilidad en origen
+	ChatMediator chat = new Telegram();
+    AbstractUser user1 = new User(chat, "ElUsuario1");
+    AbstractUser user2 = new User(chat, "ElUsuario2");
+
+
 	
-	
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+	@Test
+	@DisplayName("ChatMediator Instance OK")
+	void chatMediatorInstance() {
+		assertTrue(chat instanceof ChatMediator);		
 	}
 	
+	@Test
+	@DisplayName("User Instance OK")
+	void user1Instance() {
+		assertTrue(user1 instanceof User);		
+	}
 	
-
+	@Test
+	@DisplayName("User Instance OK")
+	void user2Instance() {	
+		assertTrue(user2 instanceof User);
+	}
+	
+	@Test
+	@DisplayName("User AbstractUser Instance OK")
+	void user1AbstractUserInstance() {
+		assertTrue(user1 instanceof AbstractUser);		
+	}
+	
+	@Test
+	@DisplayName("User AbstractUser Instance OK")
+	void user2AbstractUserInstance() {	
+		assertTrue(user2 instanceof AbstractUser);
+	}
+	
+	@Test
+    @DisplayName("user1 notNull")
+    void user1NotNull() {
+        assertNotNull(user1);
+    }
+	
+	@Test
+    @DisplayName("user1 notEqual user2 ")
+    void usersNotEquals() {
+        assertNotEquals(user1, user2);
+    }
+	
+	@Test
+    @DisplayName("Recibe el mensaje OK")
+    void test5() {
+        String mensaje = "Mensaje de bienvenida";
+        assertEquals(mensaje, user1);
+    }
 }
