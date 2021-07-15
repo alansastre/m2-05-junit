f068c60af44736ac09b76f3ec1985494f08e512e
luzdelgado <luzdivdelg@gmail.com>
2021-07-15 17:06:28 +0200

    AdapterCarTest Created

--- src/test/java/com/example/luzdelgado/AdapterCarTest.java
+++ src/test/java/com/example/luzdelgado/AdapterCarTest.java
@@ -1,5 +1,67 @@
 package com.example.luzdelgado;
 
-public class AdapterCarTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.example.demo.patterns.structural.adapter.Car;
+
+ class AdapterCarTest {
+ 
+	Car coche = new Car();
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("setUpBeforeClass");
+
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		System.out.println("tearDownAfterClass");
+
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		System.out.println("setUp");
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		System.out.println("tearDown");
+
+	}
+
+	@Test
+	@DisplayName("Test de aumento de velocidad sumando positivos")
+	void testSpeedUpPositive() {
+		double velActual = coche.getSpeed();
+		coche.speedUp(5);
+		assertEquals(velActual+5, coche.getSpeed());
+	}
+	
+	@Test
+	@DisplayName("Test de aumento de velocidad con numeros negativos")
+	void testSpeedUpNegative() {
+		double velActual = coche.getSpeed();
+		coche.speedUp(-3);
+		assertEquals(velActual-3, coche.getSpeed());
+	}
+
+//	@Test
+//	void testGetSpeed() {
+//		
+//	}
+//
+//	@Test
+//	void testSetSpeed() {
+//		
+//	}
+	
 }
