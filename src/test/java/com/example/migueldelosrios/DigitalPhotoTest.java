cb7ce20ab2982f7cf80e5f473880fe9ed8e43cd7
MiwelR <rrriosss@gmail.com>
2021-07-16 11:40:07 +0200

    Primer test DigitalPhoto

--- src/test/java/com/example/migueldelosrios/DigitalPhotoTest.java
+++ src/test/java/com/example/migueldelosrios/DigitalPhotoTest.java
@@ -2,6 +2,7 @@ package com.example.migueldelosrios;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.example.demo.patterns.structural.decorator.DigitalPhoto;
@@ -11,9 +12,16 @@ public class DigitalPhotoTest {
 	DigitalPhoto fotodigital = new DigitalPhoto();
 	
 	@Test
+	@DisplayName("Test devolver foto normal sin editar")
 	void showTest() {
 		String mostrarfoto = fotodigital.show();
 		assertEquals(mostrarfoto, "Normal photo without edit");
+	}
+	
+	@Test
+	@DisplayName("Test")
+	void costTest() {
+//		double cost = fotodigital.cost();
 		
 	}
 	
