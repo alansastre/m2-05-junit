db3a67ba1e377c5627b7be3bea66d71f7e2d3846
Ricardo <rnogueradrum@gmail.com>
2021-07-16 09:34:59 +0200

    commit

--- src/test/java/com/example/ricardonoguera/mediator/UserTest.java
+++ src/test/java/com/example/ricardonoguera/mediator/UserTest.java
@@ -2,48 +2,31 @@ package com.example.ricardonoguera.mediator;
 
 import com.example.demo.patterns.behavioral.mediator.*;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 
 class UserTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	ChatMediator chat = new Telegram();
+	User user1 = new User(chat,"ElUsuario1");
+	User user2 = new User(chat,"ElUsuario2");
+    
+    
 	@Test
-	void testSend() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testReceive() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testUser() {
-		fail("Not yet implemented");
-	}
-
+    @DisplayName("Envio")
+    void envio() {
+        chat.addUser(user1);
+        user1.send("Mensaje enviado");
+        
+        
+    }
 }
