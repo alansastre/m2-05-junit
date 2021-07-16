5bb579b3384f0253f12e76b9c763f07c57afce22
carlos <carlos@DESKTOP-J61L43O>
2021-07-16 10:34:54 +0200

    Commit dudas

--- src/test/java/com/example/JuanCarlosGarrido/TestBuilder.java
+++ src/test/java/com/example/JuanCarlosGarrido/TestBuilder.java
@@ -1,5 +1,27 @@
 package com.example.JuanCarlosGarrido;
 
-public class TestBuilder {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import com.example.demo.patterns.creational.builder.User;
+import com.example.demo.patterns.creational.builder.User.Builder;
+
+public class TestBuilder {
+	
+	@Test
+	@DisplayName("Creación de User")
+	void createUser() {
+		Builder user = new Builder();
+		user.equals(new User(2L,"","","", true));
+			
+	}
+	
+	@Test
+	@DisplayName("Creación de tipo User")
+	void createByTypeBuilder(){
+		//User Builder(); 
+		//Builder.class.; 
+	}
+	
+	
 }
