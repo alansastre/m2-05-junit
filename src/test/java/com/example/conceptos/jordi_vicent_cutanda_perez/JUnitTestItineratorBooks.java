a5fd3518017be487c025bc4a1f30bc9c65723c89
KuXarito <kingspammer@protonmail.com>
2021-07-21 22:21:41 +0200

    Arreglo de clase añadida demás

--- src/test/java/com/example/conceptos/jordi_vicent_cutanda_perez/JUnitTestItineratorBooks.java
+++ src/test/java/com/example/conceptos/jordi_vicent_cutanda_perez/JUnitTestItineratorBooks.java
@@ -1,20 +1,18 @@
 package com.example.conceptos.jordi_vicent_cutanda_perez;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import com.example.demo.patterns.behavioral.iterator.Book;
 
-import junit.framework.TestCase;
-
-@Nested
-@DisplayName("JUnitTestSampleListBooksTest")
-class JUnitTestSampleListBooksTest extends TestCase {
-	private static final String LORE_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id diam veunt praesent semper feugiat.";
+public class JUnitTestItineratorBooks {
+		private static final String LORE_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id diam veunt praesent semper feugiat.";
 
 	@Test
 	@DisplayName("Add Sample List Books")
@@ -22,12 +20,12 @@ class JUnitTestSampleListBooksTest extends TestCase {
 		int randomBooksToCreate = (int)Math.round(Math.random()*(100));
 
         List<Book> randomSampleBooks = addSampleListBooksTest(randomBooksToCreate);
-        
+
         assertEquals(randomBooksToCreate,randomSampleBooks.size());
-        
+
         //TODO Add check fields
 	}
-	
+
 	protected static List<Book> addSampleListBooksTest (int numberBooks) {
 
         List<Book> sampleListBooks =  new ArrayList<Book>();
@@ -52,67 +50,62 @@ class JUnitTestSampleListBooksTest extends TestCase {
         }
 		return sampleListBooks;
 	}
-}
 
-@Nested
-@DisplayName("JUnitTestBooksTest")
-class JUnitTestBooksTest extends TestCase {
-	
 	@Test
 	@DisplayName("Massive Itinerator")
     void testMassiveIterator() {
 
         int randomBooksToCreate = (int)Math.round(Math.random()*(100)+1);
 
-        List<Book> randomSampleBooks = JUnitTestSampleListBooksTest.addSampleListBooksTest(randomBooksToCreate);
+        List<Book> randomSampleBooks = JUnitTestItineratorBooks.addSampleListBooksTest(randomBooksToCreate);
         Iterator<Book> listBooksIterator = randomSampleBooks.iterator();
 
         for (int actualBook = 1; actualBook <= (randomSampleBooks.size()); actualBook++) {
 
         	boolean haveNextItemInList = listBooksIterator.hasNext();
-        	
+
         	System.out.println("TEST MASSIVE ITINERAOTR BOOK");
-        	
+
         	System.out.println("Actual Position: " + String.valueOf(actualBook)
             + " of "
             + String.valueOf(randomSampleBooks.size()));
 
             System.out.println(listBooksIterator.next().toString());
-            
+
             assertEquals
-            ("Position fail: "
-            + String.valueOf(actualBook)
-            + " of "
-            + String.valueOf(randomSampleBooks.size()),
-            true, haveNextItemInList);
+            (true,
+        		haveNextItemInList,"Position fail: "
+                    + String.valueOf(actualBook)
+                    + " of "
+                    + String.valueOf(randomSampleBooks.size()));
         }
 
     }
-	
+
 	@Test
 	@DisplayName("Unique Itinerator")
     void testUniqueIterator() {
 
-        List<Book> randomSampleBook = JUnitTestSampleListBooksTest.addSampleListBooksTest(1);
+        List<Book> randomSampleBook = JUnitTestItineratorBooks.addSampleListBooksTest(1);
         Iterator<Book> listBookIterator = randomSampleBook.iterator();
-            
+
         	boolean haveNextItemInList = listBookIterator.hasNext();
-        	
+
         	String nextBook = listBookIterator.next().toString();
 
             System.out.println(nextBook);
-            
-            assertEquals (nextBook, true, haveNextItemInList);
+
+            assertEquals(true, haveNextItemInList, nextBook);
     }
-	
+
 	@Test
 	@DisplayName("Null Itinerator")
     void testNullIterator() {
         List<Book> randomSampleBook = new ArrayList<Book>();
         Iterator<Book> listBookIterator = randomSampleBook.iterator();
-            
+
         	boolean haveNextItemInList = listBookIterator.hasNext();
-            
+
             assertEquals(false, haveNextItemInList);
 	}
 }
